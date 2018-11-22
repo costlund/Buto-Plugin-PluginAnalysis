8f9378b50d4818059a5f5cdcdd80de4f14410328
Claes Östlund <claesostlund@Mefromhd-MBP18.local>
2018-11-22 22:59:10 +0100

    Improvement.

--- js/function.js
+++ js/function.js
@@ -1,9 +1,17 @@
-function PluginThemeAnalysis(){
+function PluginPluginAnalysis(){
   this.analys = function(){
     PluginWfAjax.load('start', '/[[class]]/analys');
   }
+  this.plugin = function(id){
+    id = id.replace('.', '_A_DOT_');
+    PluginWfBootstrapjs.modal({id: 'modal_plugin', label: 'Plugin', url: '/plugin_analysis/plugin/id/'+id, resizable: true, footer: '', footer_btn_close: true, footer_btn_close_text: 'Close'});
+  }
+  this.manifest_create = function(element){
+    var id = element.getAttribute('data-url_id');
+    PluginWfBootstrapjs.modal({id: 'modal_manifest_create', label: 'Manifest create', url: '/plugin_analysis/manifest_create/id/'+id, resizable: true, footer: '', footer_btn_close: true, footer_btn_close_text: 'Close'});
+  }
 }
-var PluginThemeAnalysis = new PluginThemeAnalysis();
+var PluginPluginAnalysis = new PluginPluginAnalysis();
 
 
 
